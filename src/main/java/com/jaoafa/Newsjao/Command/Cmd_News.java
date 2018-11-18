5153db1def52b1a98fde10848b4e44dc7aab0cc8
Tomachi[Main] <lego@aol.jp>
2018-11-19 00:41:52 +0900

    change: 調整

--- src/main/java/com/jaoafa/Newsjao/Command/Cmd_News.java
+++ src/main/java/com/jaoafa/Newsjao/Command/Cmd_News.java
@@ -8,11 +8,15 @@ import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.handle.obj.IVoiceChannel;
+import sx.blah.discord.handle.obj.IVoiceState;
 
 public class Cmd_News {
 	public static void onCommand(IDiscordClient client, IGuild guild, IChannel channel, IUser author, IMessage message, String[] args){
-		IVoiceChannel vc = guild.getVoiceChannelByID(189377933356302336L); // General VC
-
+		IVoiceState vs = author.getVoiceStateForGuild(guild);
+		IVoiceChannel vc = vs.getChannel();
+		if(vc == null){
+			vc = guild.getVoiceChannelByID(189377933356302336L); // General VC
+		}
 		NewsSpeak.speakNews(client, channel, vc, false);
 	}
 }
